1e25e5ae7c442c1c88acc3700c931902db2ba333
luisrmendes <lrmmendes98@gmail.com>
2025-06-26 21:28:59 +0000

    fix tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets"); // copy static assets
 
   return {
+    pathPrefix: "/notEnoughBlogs/",
     dir: {
       input: ".",
       includes: "_includes",
